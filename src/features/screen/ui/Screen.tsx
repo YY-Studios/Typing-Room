05f23884b0878efc1e1d1a5f29abd91aceffb038
lywoo00 <lywoo00@naver.com>
2026-03-19 00:44:16 +0900

    feat: 타이핑 정확도, 타수 계산 로직 구현(#9)

--- src/features/screen/ui/Screen.tsx
+++ src/features/screen/ui/Screen.tsx
@@ -5,8 +5,12 @@ import { TypingStats } from './TypingStats';
 import { LanguageSelector } from '@/features/setting/LanguageSelector';
 import { TypeSelector } from '@/features/setting/TypeSelector';
 import { TypingArea } from './TypingArea';
+import { useTypingEngine } from '../hooks/useTypingEngine';
+import { useTypingSettingStroe } from '@/shared/stores/useTypingSettingStore';
 
 export default function Screen() {
+  const { selectedText } = useTypingSettingStroe();
+  const typingEngine = useTypingEngine(selectedText);
   return (
     <div className="flex flex-col gap-6 p-5 max-w-xl mx-auto">
       {/* 언어 선택 */}
@@ -14,8 +18,8 @@ export default function Screen() {
       {/* 타입 선택 */}
       <TypeSelector />
       {/* 타이핑 피드백/몰입 요소 */}
-      <TypingStats />
-      <TypingArea />
+      <TypingStats accuracy={typingEngine.accuracy} cpm={typingEngine.cpm} />
+      <TypingArea typingEngine={typingEngine} />
     </div>
   );
 }
